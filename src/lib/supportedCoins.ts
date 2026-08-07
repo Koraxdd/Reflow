87e4efaff269ddf26b80a6cc47fcc268d67af458
koraxd <bergariusz@gmail.com>
2026-08-07 09:49:14 +0100

    feat: added supported coins list

---
+++ src/lib/supportedCoins.ts
@@ -0,0 +1,123 @@
+type SupportedCoin = {
+    symbol: string
+    name: string
+    binanceSymbol: string
+    logo: string
+}
+
+export const supportedCoins: SupportedCoin[] = [
+    {
+        symbol: "BTC",
+        name: "Bitcoin",
+        binanceSymbol: "btcusdt",
+        logo: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+    },
+    {
+        symbol: "ETH",
+        name: "Ethereum",
+        binanceSymbol: "ethusdt",
+        logo: "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
+    },
+    {
+        symbol: "USDT",
+        name: "Tether",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/325/large/Tether.png?1696501661",
+    },
+    {
+        symbol: "BNB",
+        name: "BNB",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970",
+    },
+    {
+        symbol: "USDC",
+        name: "USDC",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/6319/large/USDC.png?1769615602",
+    },
+    {
+        symbol: "XRP",
+        name: "XRP",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1696501442",
+    },
+    {
+        symbol: "SOL",
+        name: "Solana",
+        binanceSymbol: "solusdt",
+        logo: "https://coin-images.coingecko.com/coins/images/4128/large/solana.png?1718769756",
+    },
+    {
+        symbol: "ADA",
+        name: "Cardano",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/975/large/cardano.png?1696502090",
+    },
+    {
+        symbol: "LINK",
+        name: "Chainlink",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/877/large/Chainlink_Logo_500.png?1760023405",
+    },
+    {
+        symbol: "AVAX",
+        name: "Avalanche",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/12559/large/Avalanche_Circle_RedWhite_Trans.png?1696512369",
+    },
+    {
+        symbol: "SUI",
+        name: "Sui",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/26375/large/sui-ocean-square.png?1727791290",
+    },
+    {
+        symbol: "DOGE",
+        name: "Dogecoin",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/5/large/dogecoin.png?1696501409",
+    },
+    {
+        symbol: "TRX",
+        name: "TRON",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/1094/large/photo_2026-04-13_09-59-16.png?1776048311",
+    },
+    {
+        symbol: "HYPE",
+        name: "Hyperliquid",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/50882/large/hyperliquid.jpg?1729431300",
+    },
+    {
+        symbol: "ZEC",
+        name: "Zcash",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/486/large/Brandmark-Yellow_%281%29.png?1785810558",
+    },
+    {
+        symbol: "XMR",
+        name: "Monero",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/69/large/monero_logo.png?1696501460",
+    },
+    {
+        symbol: "XLM",
+        name: "Stellar",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/100/large/fmpFRHHQ_400x400.jpg?1735231350",
+    },
+    {
+        symbol: "DAI",
+        name: "Dai",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/9956/large/Badge_Dai.png?1696509996",
+    },
+    {
+        symbol: "LTC",
+        name: "Litecoin",
+        binanceSymbol: "",
+        logo: "https://coin-images.coingecko.com/coins/images/2/large/litecoin.png?1696501400",
+    },
+]
